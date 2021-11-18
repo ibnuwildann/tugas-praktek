382e346013435c47e24988db70231485c70116d9
Ibnu Wildan <60096775+ibnuwildann@users.noreply.github.com>
2021-11-18 19:11:23 +0700

    Add files via upload

---
+++ pos/config.php
@@ -0,0 +1,21 @@
+<?php
+
+date_default_timezone_set("Asia/Jakarta");
+error_reporting(0);
+
+	// 
+	$host 	= 'localhost'; // host server
+	$user 	= 'root';  // username server
+	$pass 	= ''; // 
+	$dbname = 'db_toko'; 
+	
+	try{
+		$config = new PDO("mysql:host=$host;dbname=$dbname;", $user,$pass);
+		//echo 'sukses';
+	}catch(PDOException $e){
+		echo 'KONEKSI GAGAL' .$e -> getMessage();
+	}
+	
+	$view = 'fungsi/view/view.php'; // direktori fungsi select data
+?>
+
